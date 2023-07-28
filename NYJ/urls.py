4b6731362b840be27342ee87fad8a83f2dce28dc
Brian Quilinan <81407102+Brian150798@users.noreply.github.com>
2023-07-28 09:47:35 -0400

    test3

--- NYJ/urls.py
+++ NYJ/urls.py
@@ -1,4 +1,4 @@
-"""LavadoExpress URL Configuration
+"""NYJ URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -13,10 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls import handler404
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('core.urls')),
 ]
+
+handler404 = 'core.views.error_404'
--- core/static/core/js/home.js
+++ core/static/core/js/home.js
@@ -1,25 +1,48 @@
 
-/* FUNCION PARA CAMBIAR DE COLOR LA BARRA DE NAVEGACION AL REALIZAR SCROLL*/
-var nav = document.getElementById('navbar');
-var letranav1 = document.getElementById('letramenu');
-var letranav2 = document.getElementById('letramenu2');
-var letranav3 = document.getElementById('letramenu3');
-var letranav4 = document.getElementById('letramenu4');
 
-window.addEventListener('scroll',function(){
-    if(this.window.pageYOffset > 200){
-        nav.classList.add('bg-primary','shadow');
-        letranav1.style.color = 'beige';
-        letranav2.style.color = 'beige';
-        letranav3.style.color = 'beige';
-        letranav4.style.color = 'beige';
-     
-    }else{
-        nav.classList.remove('bg-primary','shadow');
-        letranav1.style.color = '#141515';
-        letranav2.style.color = '#141515';
-        letranav3.style.color = '#141515';
-        letranav4.style.color = '#141515';
-    }
+
+
+/* FUNCION PARA CAMBIAR DE COLOR LA BARRA DE NAVEGACION AL REALIZAR SCROLL - HOVER*/
+$(document).ready(function () {
+
+
+    $('.imgservi').hide();
+
+    var nav = document.getElementById('navbar');
+
+    window.addEventListener('scroll',function(){
+        if(this.window.pageYOffset > 190){
+            nav.classList.add('shadow');
+            $(".menu").css({'background-color': ' #0f6ec1'});
+            $('.enlace').css({'-webkit-text-stroke': '0px black'});
+            $('.enlace').css({'color': '#f1eeee'});
+        }else{ 
+            nav.classList.remove('shadow');
+            $(".menu").css({'background-color': ' #5555d600'});
+            $('.enlace').css({'-webkit-text-stroke': '0.5px black'});
+            $('.enlace').css({'color': '#010101'});
+        }
+
+        if(this.window.pageYOffset >= 600){
+            $('.imgservi').fadeIn();
+
+        }else{
+            $('.imgservi').fadeOut();
+        }
+
+});
+//------------------------------------------------------------------------
+
+
 });
 
+
+
+let quienes_somos = document.getElementById('somos');
+
+let texto_quienessomos =`<p class="textoquienessomos" data-aos="fade-up"
+data-aos-anchor-placement="center-bottom">Una pareja emprendedora que su meta es dar el mejor servicio en limpieza,
+lavanderia e incluso carpinteria. Estamos comprometidos con nuestro trabajo y dar el mejor servicio, por lo que en 2022 
+decidimos crear N&J LavadoExpress. </p>`;
+
+quienes_somos.innerHTML = texto_quienessomos;
